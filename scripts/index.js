87519cfb910e9a586d8774722d9495e54f339362
skylar-baehmn <skylarbachman@gmail.com>
2024-09-19 17:08:14 -0500

    Add files via upload
    Added the base code to display an image before the homepage is loaded. Will replace .PNG with .gif as soon as I finish it, which could be a while

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,20 @@
 $(document).ready( () => {
+    // our trusty sleep function
+    function sleep(tick) {
+        return new Promise(resolve => setTimeout(resolve, tick));
+    }
+
+    // we want to display a gif before we display the actual website contents (it may temporarily be an image, I need to finish the gif)
+    async function show_homepage_gif() {  
+        // wait for sleep, then change so that content is displayed and overflow is no longer hidden
+        await sleep(5000);
+        document.getElementById('homepage_gif').hidden = true;
+        document.body.style.overflow = 'shown';
+        document.getElementById('homepage_content').hidden = false;
+    }
+
+    show_homepage_gif();
+
     // event listener for buttons
     $('#hero_box_form_button').click( () => {
         window.location.href = 'worlds_best_form.html';
